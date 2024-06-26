0b29335cc859762dd89f5b73f080e9400d6b3508
proggeler <bas@dmt-software.nl>
2024-06-26 20:57:06 +0200

    #2: added middleware to decompress gzipped response

---
+++ src/Middleware/ZlibInflateMiddleware.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace DMT\Http\Client\Middleware;
+
+use DMT\Http\Client\MiddlewareInterface;
+use DMT\Http\Client\RequestHandlerInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+
+/**
+ * Middleware to decompress a gzip (.gz) compressed response.
+ */
+class ZlibInflateMiddleware implements MiddlewareInterface
+{
+    private StreamFactoryInterface $factory;
+
+    public function __construct(StreamFactoryInterface $factory)
+    {
+        if (!extension_loaded('zlib')) {
+            trigger_error('module zlib is not installed', E_USER_ERROR);
+        }
+
+        $this->factory = $factory;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function process(RequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $response = $handler->handle($request);
+
+        if ($response->getBody()->tell() != 0) {
+            $response->getBody()->rewind();
+        }
+
+        if (strpos($response->getHeaderLine('Content-Type'), 'application/x-gzip-compressed') !== false) {
+            $resource = $response->getBody()->detach();
+
+            stream_filter_append($resource, 'zlib.inflate', STREAM_FILTER_READ, ['window' => 31, 'memory' => 9]);
+
+            return $response->withBody(
+                $this->factory->createStreamFromResource($resource)
+            );
+        }
+
+        return $response;
+    }
+}
---
+++ tests/Middleware/ZlibInflateMiddlewareTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace DMT\Test\Http\Client\Middleware;
+
+use DMT\Http\Client\Middleware\ZlibInflateMiddleware;
+use DMT\Http\Client\RequestHandler;
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
+use Http\Factory\Guzzle\StreamFactory;
+use PHPUnit\Framework\TestCase;
+
+class ZlibInflateMiddlewareTest extends TestCase
+{
+    public function testProcess(): void
+    {
+        $expected = '<html><body>html snippet</body></html>';
+
+        $handle = fopen('php://temp', 'w+');
+        fwrite($handle, zlib_encode($expected, ZLIB_ENCODING_GZIP));
+        rewind($handle);
+
+        $request = new Request('GET', '/');
+        $client = new Client([
+            'handler' => HandlerStack::create(
+                new MockHandler([
+                    (new Response(200))
+                        ->withBody((new StreamFactory())->createStreamFromResource($handle))
+                        ->withHeader('Content-Type', 'application/x-gzip-compressed'),
+                ])
+            )
+        ]);
+
+        $handler = new RequestHandler($client, new ZlibInflateMiddleware(new StreamFactory()));
+
+        $this->assertSame($expected, $handler->handle($request)->getBody()->getContents());
+    }
+}
